2aa45d689f0fcfb253ecec40437271d06b3f96da
veena14cs <veena@bitjini.com>
2016-03-02 18:06:00 +0530

    working on screens

--- EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Played_File.java
+++ EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Played_File.java
@@ -2,25 +2,26 @@ package com.example.bitjini.efficientbrainy;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
-
-    Button splashBtn;
+/**
+ * Created by bitjini on 2/3/16.
+ */
+public class Played_File extends Activity {
+    Button stopExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.played_file);
 
-        splashBtn=(Button)findViewById(R.id.splashbtn);
+        stopExit=(Button)findViewById(R.id.stopExit);
 
-        splashBtn.setOnClickListener(new View.OnClickListener() {
+        stopExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,Login.class);
+                Intent intent=new Intent(Played_File.this,PlayList.class);
                 startActivity(intent);
             }
         });
