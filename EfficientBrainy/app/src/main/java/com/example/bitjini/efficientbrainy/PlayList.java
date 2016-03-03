67152bfdce0d3fcd313f2a4eaa3d6036d3861b49
veena14cs <veena@bitjini.com>
2016-03-03 13:21:50 +0530

    Changed from activity to fragments

--- EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/PlayList.java
+++ EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/PlayList.java
@@ -3,31 +3,39 @@ package com.example.bitjini.efficientbrainy;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 /**
  * Created by bitjini on 2/3/16.
  */
-public class PlayList extends Activity implements View.OnClickListener {
+public class PlayList extends Fragment implements View.OnClickListener {
 
     Button play1,play2,play3,play4;
+    View playView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.play_list);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        play1=(Button)findViewById(R.id.play1);
-        play2=(Button)findViewById(R.id.play2);
-        play3=(Button)findViewById(R.id.play3);
-        play4=(Button)findViewById(R.id.play4);
+        playView = inflater.inflate(R.layout.play_list, container, false);
+
+
+        play1=(Button)playView.findViewById(R.id.play1);
+        play2=(Button)playView.findViewById(R.id.play2);
+        play3=(Button)playView.findViewById(R.id.play3);
+        play4=(Button)playView.findViewById(R.id.play4);
 
         play1.setOnClickListener(this);
         play2.setOnClickListener(this);
         play3.setOnClickListener(this);
         play4.setOnClickListener(this);
 
-
+return playView;
 
     }
 
@@ -36,20 +44,51 @@ public class PlayList extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.play1:
-                     Intent intent = new Intent(PlayList.this, Otp.class);
-                      startActivity(intent);
-                    break;
+                Fragment newfragment = new Otp();
+                // get the id of fragment
+                FrameLayout contentView1 = (FrameLayout) playView.findViewById(R.id.playListFrame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager1 = getFragmentManager();
+                fragmentManager1.beginTransaction()
+                        .replace(contentView1.getId(), newfragment).addToBackStack(contentView1.toString())
+                        .commit();
+
+                break;
             case R.id.play2:
-                Intent intent2 = new Intent(PlayList.this, Otp.class);
-                startActivity(intent2);
+                Fragment newfragment2 = new Otp();
+                // get the id of fragment
+                FrameLayout contentView2 = (FrameLayout) playView.findViewById(R.id.playListFrame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager2 = getFragmentManager();
+                fragmentManager2.beginTransaction()
+                        .replace(contentView2.getId(), newfragment2).addToBackStack(contentView2.toString())
+                        .commit();
+
                 break;
             case R.id.play3:
-                Intent intent3 = new Intent(PlayList.this, Otp.class);
-                startActivity(intent3);
+                Fragment newfragment3 = new Otp();
+                // get the id of fragment
+                FrameLayout contentView3 = (FrameLayout) playView.findViewById(R.id.playListFrame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager3 = getFragmentManager();
+                fragmentManager3.beginTransaction()
+                        .replace(contentView3.getId(), newfragment3).addToBackStack(contentView3.toString())
+                        .commit();
+
                 break;
             case R.id.play4:
-                Intent intent4 = new Intent(PlayList.this, Otp.class);
-                startActivity(intent4);
+                Fragment newfragment4 = new Otp();
+                FrameLayout contentView4 = (FrameLayout) playView.findViewById(R.id.playListFrame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager4 = getFragmentManager();
+                fragmentManager4.beginTransaction()
+                        .replace(contentView4.getId(), newfragment4).addToBackStack(contentView4.toString())
+                        .commit();
+
                 break;
 
         }
--- EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Played_File.java
+++ EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Played_File.java
@@ -3,28 +3,42 @@ package com.example.bitjini.efficientbrainy;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 /**
  * Created by bitjini on 2/3/16.
  */
-public class Played_File extends Activity {
+public class Played_File extends Fragment {
     Button stopExit;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.played_file);
+    View fileView;
 
-        stopExit=(Button)findViewById(R.id.stopExit);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        fileView = inflater.inflate(R.layout.played_file, container, false);
+
+        stopExit=(Button)fileView.findViewById(R.id.stopExit);
 
         stopExit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Played_File.this,PlayList.class);
-                startActivity(intent);
+               Fragment newfragment = new PlayList();
+                // get the id of fragment
+                FrameLayout contentView1 = (FrameLayout) fileView.findViewById(R.id.file_frame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager1 = getFragmentManager();
+                fragmentManager1.beginTransaction()
+                        .replace(contentView1.getId(), newfragment).addToBackStack(contentView1.toString())
+                        .commit();
             }
         });
-
+return fileView;
     }
 }
