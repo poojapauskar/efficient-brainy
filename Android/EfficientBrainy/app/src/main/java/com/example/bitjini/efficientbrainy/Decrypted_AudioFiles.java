bf5e36fad498b7c656f982a3e3e82422fb1ac80e
veena14cs <veena@bitjini.com>
2016-03-04 13:35:04 +0530

    working on audio files(2)

--- Android/EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Decrypted_AudioFiles.java
+++ Android/EfficientBrainy/app/src/main/java/com/example/bitjini/efficientbrainy/Decrypted_AudioFiles.java
@@ -1,6 +1,7 @@
 package com.example.bitjini.efficientbrainy;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
@@ -33,7 +34,7 @@ public class Decrypted_AudioFiles extends Activity{
     MediaPlayer mediaPlayer=new MediaPlayer();
 
 
-    byte [] decrpt;
+    public byte [] decrpt=new byte[]{};
 
     Context ctx;
 
@@ -50,104 +51,22 @@ public class Decrypted_AudioFiles extends Activity{
         ctx = this;
 
 
-        // decrypt the file here first argument is key and second is encrypted file which we get from SD card.
+//        // decrypt the file here first argument is key and second is encrypted file which we get from SD card.
+//
+//        try {
+//            decrpt = decrypt(KEY, getAudioFile());
+//
+//
+//           playMp3(decrpt);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-        try {
-            decrpt = decrypt(KEY, getAudioFile());
-
-             //play decrypted audio file.
-           playMp3(decrpt);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // getting the audio file from assets folder
-    public byte[] getAudioFile() throws FileNotFoundException
-
-    {
-
-        byte[] audio_data = null;
-
-        byte[] inarry = null;
-
-
-
-        AssetManager am = ctx.getAssets();
-
-        try {
-            // retrieve audio file sent by Otp class
-            String audio=getIntent().getStringExtra("audio");
-
-          InputStream is = am.open(audio);
-            int length = is.available();
-
-            audio_data = new byte[length];
-
-
-            is.read(audio_data);
-
-            is.close();
-
-            inarry = audio_data;
-            Log.e(" inarry",""+ inarry);
-
-
-        } catch (IOException e) {
-
-            // TODO Auto-generated catch block
-
-            e.printStackTrace();
-
-        }
-        return inarry;
 
     }
 
 
-    /**
-
-     * This Method is used to play audio file after decrepting.
-
-     * @param mp3SoundByteArray : This is our audio file which will be play and it converted in byte array.
-
-     */
-
-    public void playMp3(byte[] mp3SoundByteArray) {
-
-        try {
-
-            // create temp file that will hold byte array
-
-            File tempMp3 = File.createTempFile("kurchina", "mp3",getCacheDir());
-            Log.e(" tempMp3", "" + tempMp3);
-            tempMp3.deleteOnExit();
-
-            FileOutputStream fos = new FileOutputStream(tempMp3);
-            Log.e(" file output stream",""+fos.toString());
-            fos.write(mp3SoundByteArray);
-
-            fos.close();
-
-            FileInputStream fis = new FileInputStream(tempMp3);
-            Log.e(" file output stream",""+fis.getFD());
-            mediaPlayer.setDataSource(fis.getFD());
-
-            Log.e(" mediaPlayer",""+mediaPlayer);
-
-            mediaPlayer.prepare();
-
-            mediaPlayer.start();
-
-        } catch (IOException ex) {
-
-
-
-            ex.printStackTrace();
-
-        }
-
-    }
     public  byte[] decrypt(String seed, byte[] encrypted) throws Exception {
 
         byte[] rawKey = getRawKey(seed.getBytes());
@@ -161,6 +80,7 @@ public class Decrypted_AudioFiles extends Activity{
         return result;
 
     }
+
 
     //pass this audio file for encryption .The below code show process of encryption
 //Generate a key for encryption :
